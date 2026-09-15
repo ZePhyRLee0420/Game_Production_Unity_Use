b8b06d1fdba6dc735c393d263997254077a31164
katusando7 <ryosuke1912@icloud.com>
2026-09-15 19:02:10 +0900

    変更

--- Assets/Scripts/BossFlag.cs
+++ Assets/Scripts/BossFlag.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BossFlag : MonoBehaviour
 {
+    bool bossFlag = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,7 @@ public class BossFlag : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject gameObject = 
+
     }
 }
